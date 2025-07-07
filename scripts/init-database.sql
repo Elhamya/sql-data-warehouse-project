5f552659069d4fe560a6fa7ef43cda65660026bc
Elhamya <79582728+Elhamya@users.noreply.github.com>
2025-07-07 15:12:07 +1000

    Update init-database.sql

--- scripts/init-database.sql
+++ scripts/init-database.sql
@@ -6,34 +6,37 @@ Script Purpose:
     This script creates a new database named 'DataWarehouse' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'bronze', 'silver', and 'gold'.
-
+	
 WARNING:
     Running this script will drop the entire 'DataWarehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
 
+USE master;
+GO
+
 -- Drop and recreate the 'DataWarehouse' database
-BEGIN TRY
-    USE master;
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 
-    IF EXISTS (SELECT 1 FROM sys.databases WHERE name = N'DataWarehouse')
-    BEGIN
-        ALTER DATABASE [DataWarehouse] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-        DROP DATABASE [DataWarehouse];
-    END;
-    
 -- Create the 'DataWarehouse' database
-    CREATE DATABASE [DataWarehouse];
-    USE [DataWarehouse];
-    
--- Create Schemas
-    CREATE SCHEMA [bronze];
-    CREATE SCHEMA [silver];
-    CREATE SCHEMA [gold];
+CREATE DATABASE DataWarehouse;
+GO
 
-    PRINT 'Database and schemas created successfully.';
-END TRY
-BEGIN CATCH
-    PRINT 'An error occurred: ' + ERROR_MESSAGE();
-END CATCH
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
