cd963624ec52e4ddf91f703926d53a2018d96ebd
Elhamya <79582728+Elhamya@users.noreply.github.com>
2025-07-18 11:17:55 +1000

    Update data_segmentation_analysis.sql

--- scripts/Advanced_Data_Analytics/data_segmentation_analysis.sql
+++ scripts/Advanced_Data_Analytics/data_segmentation_analysis.sql
@@ -1,10 +1,35 @@
+/*
+===============================================================================
+Data Segmentation Analysis
+===============================================================================
+Purpose:
+-	Group data into defined ranges or categories by transforming a measure 
+    into grouped segments.
+-	This allows for aggregation based on the newly created categories.
+
+Use Cases:
+-	Customer segmentation
+-	Product categorization
+-	Regional or geographic analysis
+-   ...
+
+Example Concept:
+-	[Measure] BY [Measure], where one measure is transformed into a range 
+    or group to enable aggregation.
+
+SQL Functions Used:
+-	CASE WHEN: Defines custom segmentation logic.
+-	GROUP BY: Aggregates data based on the defined segments.
+===============================================================================
+*/
+
 
 /*
 Group customers into three segments based on their spending behavior:
     - VIP: Customers with at least 12 months of history and spending more than €5,000.
     - Regular: Customers with at least 12 months of history but spending €5,000 or less.
-    - New: Customers with a lifespan less than 12 months.
-Return the total number of customers by each group.
+    - New: Customers with a lifespan of less than 12 months.
+Return the total number of customers for each group.
 */
 
 WITH customer_order_summary AS (
